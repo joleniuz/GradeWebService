b81d77f53ec5c4cb1650a9d36bbf7f5afda92c5a
joleniuz <49279283+joleniuz@users.noreply.github.com>
2020-11-16 15:24:44 +0100

    Uppdaterade newStudentByForm i CanvasAPI

--- src/main/java/canvas/CanvasDAO.java
+++ src/main/java/canvas/CanvasDAO.java
@@ -21,6 +21,8 @@ public class CanvasDAO {
     
     private final String GET_STUDENTS = "SELECT * FROM canvas";
     private final String GET_STUDENTS_BY_ID = "SELECT * FROM canvas WHERE studentid=?";
+    private final String ADD_STUDENT = "INSERT INTO canvas (StudentId, Omdöme, Namn, Kurskod)"
+            + "VALUES(?, ?, ?, ?) ";
     
     Connection con = null;
     PreparedStatement ps = null;
@@ -78,6 +80,40 @@ public class CanvasDAO {
         }catch(Exception e){
             e.printStackTrace();
         }
+        
+        return student;
+    }
+    
+    public CanvasDTO addStudent(String studentId, String omdöme, String namn,  String kurskod){
+        
+        CanvasDTO student = null;
+        JdbcCon db = new JdbcCon();
+        int status = 0;
+        
+        try{
+            DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+            con = db.openConnection("canvasdb");
+            ps = con.prepareStatement(ADD_STUDENT);
+            ps.setString(1, studentId);
+            ps.setString(2, omdöme);
+            ps.setString(3, namn);
+            ps.setString(4, kurskod);
+            status = ps.executeUpdate();
+            
+            /*if(rs.next()){
+                student = new CanvasDTO();
+                student.setStudentId(rs.getString(1));
+                student.setOmdöme(rs.getString(2));
+                student.setNamn(rs.getString(3));
+                student.setKurskod(rs.getString(4));
+            
+            }*/
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        
+        
         
         return student;
     }
