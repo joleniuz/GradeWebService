fb2183e85dbb4adcd288b6be923e0b8dbc530cc2
joleniuz <49279283+joleniuz@users.noreply.github.com>
2020-11-19 15:29:24 +0100

    hej

--- src/main/java/canvas/CanvasDAO.java
+++ src/main/java/canvas/CanvasDAO.java
@@ -154,10 +154,8 @@ public class CanvasDAO {
         StudentGradeDTO studentGrade = null;
         JdbcCon db = new JdbcCon();
         int status = 0;
-        System.out.println("1");
         
         try{
-            System.out.println("2");
             DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
             con = db.openConnection("ladokdb");
             //persnr, namn, kurskod, modul, datum, betyg, status
@@ -175,7 +173,6 @@ public class CanvasDAO {
         }catch(Exception e){
             e.printStackTrace();
         }
-            System.out.println("hej");
      
         return studentGrade;
     }
